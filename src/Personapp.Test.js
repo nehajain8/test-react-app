5071addf512414f23408bca5df12b644445b997c
nehajain8 <35501045+nehajain8@users.noreply.github.com>
2018-06-29 00:45:19 +0530

    deleted person old file

--- src/Personapp.Test.js
+++
@@ -1,9 +0,0 @@
-import ShallowRenderer from 'react-test-renderer/shallow';
-import Person from './person.js';
-// in your test:
-const renderer = new ShallowRenderer();
-renderer.render(<Person />);
-const result = renderer.getRenderOutput();
-
-//expect(result.type).toBe('div');
-expect(result.props.children[3].props.onClick).toEqual(increment);
